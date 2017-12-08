7d6dde3f3acf7ab2c07df75cc283516a2f9f8ea6
aniknagato <aniknagato@gmail.com>
2017-12-08 13:34:53 -0600

    Create total_distance_covered.py

---
+++ core/feature/total_distance_covered.py
@@ -0,0 +1,5 @@
+# This feature will compute the total distance covered in a time interval
+#author : Soujanya Chatterjee
+#Description: This feature will compute the total distance covered in a time interval ( 14 days suggested in Trajectories of Depression,Ubicomp'15)
+# INPUTS : clustered centroids from Soujanya
+# OUTPUT : total distance covered in a time interval
