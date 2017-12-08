559a7af988c1330306d73c555122b616bd9e208d
mhwlader <shiplu.cse.du@gmail.com>
2017-12-07 20:40:51 -0600

    Added some methods (not all) that I am going to write. Tried to add all the streams I need. - Shiplu

--- core/feature/phone.py
+++ core/feature/phone.py
@@ -1,1 +1,27 @@
-# shiplu
+# 1. Total / Average screen tap count
+#       input: phone touch screen stream
+#       output: total / average
+#
+# 2. Average inter phone call time
+#       input: phone call log stream
+#       output: time in second
+#
+# 3. Average inter text / sms time
+#       input: phone sms log stream
+#       output time in second
+#
+# 4. total notification count
+#       input: phone notification stream
+#       output: integer
+#
+# 5. Average proximity
+#       input: phone proximity sensor stream
+#       output: float
+#
+# 6. Average ambient light
+#       input: phone ambient light sensor stream
+#       output: float
+#
+# 7. Average pressure sensed in phone
+#       input: phone pressure sensor stream
+#       output: float
