d756e8b4af97abbe4796b8c680c874917249962d
vagrant <anandatirtha@gmail.com>
2018-02-09 20:53:48 +0000

    CC-439 Enabled syslog based logging

--- core/driver.py
+++ core/driver.py
@@ -1,23 +1,57 @@
+# Copyright (c) 2017, MD2K Center of Excellence
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import sys
 import os
 import utils.config
 import argparse
+import syslog
+
+from syslog import LOG_ERR
 
 
-# ADD ADMISSION CoNTROL LOGIC
+# TODO ADD ADMISSION CoNTROL LOGIC
 
+# Initialize logging
+syslog.openlog(ident="CerebralCortex-Driver")
 
-
+'''
+This method runs the processing pipeline for each of
+the features in the list.
+'''
 def process_features(feature_list):
+# TODO FIXME - should we parallize these as spark jobs ?
     for module in feature_list:
         feature_class_name = getattr(module,'feature_class_name')
         feature_class = getattr(module,feature_class_name)
         feature_class_instance = feature_class()
         feature_class_instance.process()
 
-
-
-def import_features(feature_list):
+'''
+This method discovers all the features that are present.
+'''
+def discover_features(feature_list):
     feature_dir = os.path.join(utils.config.FEATURES_DIR_NAME)
     found_features = []
     if feature_list:
@@ -28,40 +62,40 @@ def import_features(feature_list):
     for subdir in feature_subdirs:
         feature = os.path.join(feature_dir,subdir)
         if not os.path.exists(feature):
-            print('Feature not found %s.' % subdir)
+            syslog.syslog(LOG_ERR,'Feature not found %s.' % subdir)
             continue
         if os.path.isdir(feature):
-            print('Found feature %s' % feature)
+            syslog.syslog('Found feature %s' % feature)
             sys.path.append(feature)
             try:
                 module = __import__(subdir)
                 found_features.append(module)
             except Exception as exp:
-                print exp
-        print('AAAAAAAAAA')
+                syslog.syslog(LOG_ERR,exp)
     
     return found_features
     
 
+'''
+This method returns the execution order of processing the features 
+after resolving the inter dependencies.
+'''
 def generate_feature_processing_order(feature_list):
     return feature_list
+
 
 def main():
     # Get the list of the features to process
     parser = argparse.ArgumentParser(description='CerebralCortex Feature Processing Driver')
     parser.add_argument("-f", "--feature-list", help="List of feature names seperated by commas", required=False)
     args = vars(parser.parse_args())
-    print('args %s' % args)
     feature_list = None
     if args['feature_list']:
         feature_list = args['feature_list'].split(',')
     
-    found_features = import_features(feature_list)
+    found_features = discover_features(feature_list)
     feature_to_process = generate_feature_processing_order(found_features)
     #process_features(feature_to_process)
-
-        
-
     
 if __name__ == '__main__':
     main()
