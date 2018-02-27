5f284aaea1093d1a67c8a5fa2dbe938aa2f754a8
sakther <sakther@memphis.edu>
2018-02-27 14:59:33 -0600

    Initial commit for activity and posture detection

---
+++ core/feature/activity/do_classification.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2018, MD2K Center of Excellence
+# - Sayma Akther <sakther@memphis.edu>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from cerebralcortex.core.datatypes.datastream import DataStream
+from cerebralcortex.core.datatypes.datastream import DataPoint
+from core.markers.activity.import_model_files import get_posture_model, get_activity_model
+
+
+def classify_posture(features: DataStream) -> DataStream:
+
+    clf = get_posture_model()
+    labels = []
+    for dp in features.data:
+        preds=clf.predict([dp.sample])
+        labels.append(DataPoint(start_time=dp.start_time, end_time=dp.end_time, sample=preds))
+
+    label_stream = DataStream.from_datastream([features])
+    label_stream.data = labels
+
+    return labels
+
+def classify_activity(features: DataStream) -> DataStream:
+
+    clf = get_activity_model()
+    labels = []
+    for dp in features.data:
+        preds=clf.predict([dp.sample])
+        labels.append(DataPoint(start_time=dp.start_time, end_time=dp.end_time, sample=preds))
+
+    label_stream = DataStream.from_datastream([features])
+    label_stream.data = labels
+
+    return labels
+
+
+
+
+
