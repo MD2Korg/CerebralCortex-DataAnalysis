f682b9e90d7a1d5894d0cd9ac66a28d83823d9b5
sakther <sakther@memphis.edu>
2018-03-07 16:24:02 -0600

    Update activity detection module with

--- core/feature/activity/ACTIVITY_CONSTANTS.py
+++ core/feature/activity/ACTIVITY_CONSTANTS.py
@@ -23,7 +23,8 @@ ACTIVITY_MODEL_FILENAME = 'core/feature/activity/models/activity_level_randomfor
 
 # Output labels
 ACTIVITY_LABELS = ["NO", "LOW", "WALKING", "MOD", "HIGH"]
-POSTURE_LABELS = ["LYING", "SITTING", "STANDING"]
+POSTURE_LABELS = ["lying", "sitting", "standing"]
 
 ACTIVITY_LABELS_INDEX_MAP = {"NO": 0, "LOW": 1, "WALKING": 2, "MOD": 3, "HIGH": 4}
-POSTURE_LABELS_INDEX_MAP = {"LYING": 0, "SITTING": 1, "STANDING": 2}
+POSTURE_LABELS_INDEX_MAP = {"lying": 0, "sitting": 1, "standing": 2}
+
