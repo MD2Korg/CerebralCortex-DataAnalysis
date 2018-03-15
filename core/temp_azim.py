689ad49227f6564e754e0d9ff1c57c11ac3574fc
azim <aungkonazim@gmail.com>
2018-03-14 22:33:52 -0500

    just temporary file added

---
+++ core/temp_azim.py
@@ -0,0 +1,11 @@
+from cerebralcortex.cerebralcortex import CerebralCortex
+from pprint import pprint
+from datetime import timedelta
+import numpy as np
+import pandas as pd
+import os
+import pickle
+CC = CerebralCortex()
+users = CC.get_all_users("mperf-alabsi")
+for user in users:
+    print(user['identifier'])
