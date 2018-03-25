afe4a8b3c60b7602537130922bd05cce5b110953
Mithun Saha <amimithun@gmail.com>
2018-03-25 14:12:45 -0500

    Typying detection code

--- core/feature/typing/typing.py
+++ core/feature/typing/typing.py
@@ -43,18 +43,21 @@ motionsense_hrv_gyro_left = "GYROSCOPE--org.md2k.motionsense--MOTION_SENSE_HRV--
 
 class TypingMarker(ComputeFeatureBase):
     """
-    Detects Typing activity: Provides Start time and End-time of a typing session.
-    The typing session starts when the participant starts typing and ends if no key
-    is pressed for more than 10 seconds. The inference is made from the acc and gyro values of both motionsense wrist bands.
+    Detects Typing activity: Provides Start time and End-time of typing sessions.
+    The typing sessions starts when the participant starts typing and ends when
+    no key is pressed for more than 10 seconds. The inference is made from the
+    accl and gyro values of both motionsense wrist bands.
 
     """
 
-    # def __init__(self):
-    #     CC_CONFIG_PATH = '/home/md2k/cc_configuration.yml'
-    #     self.CC = CerebralCortex(CC_CONFIG_PATH)
-
     def collect_data(self, dict, day, user_id):
-        # this function colects user data of all stream ids for a common day
+        """
+        This function collects user data of all stream ids for a day
+        :param dict: a dictionary of all stream ids with days
+        :param user_id:
+        :return: all datapoints of the day if any stream id has data
+                for that day
+        """
         all_data = []
         for stream_id in dict:
             if day in dict[stream_id]:
@@ -66,37 +69,52 @@ class TypingMarker(ComputeFeatureBase):
         return all_data
 
     def get_common_days(self, user_id):
+        """
+         This function gets both wrists' accl and gyro steam ids with dates to
+         extract commons days for which both both wrists' accel and gyro
+         data exist. Also creates left,right accl and gyro streams' dictionaries
+         of stream ids, containing all the dates of each stream id.         .
+        :param user_id:
+        :return: list of common days and four dictionaries for
+                left, right acc and gyro
+        """
+
         accel_right_stream_ids_with_date = {}
         gyro_right_stream_ids_with_date = {}
         accel_left_stream_ids_with_date = {}
         gyro_left_stream_ids_with_date = {}
 
-        streams = self.CC.get_user_streams(user_id)  # gets all streams of one user
+        # gets all streams of one user
+        streams = self.CC.get_user_streams(user_id)
 
         if streams:
-            for s in [motionsense_hrv_accel_right, motionsense_hrv_gyro_right, motionsense_hrv_accel_left,
+            for s in [motionsense_hrv_accel_right, motionsense_hrv_gyro_right,
+                      motionsense_hrv_accel_left,
                       motionsense_hrv_gyro_left]:
 
-                stream_id_all = self.CC.get_stream_id(user_id,
-                                                      s)  # gets a list of dictionary of all stream ids of one stream
-
+                stream_id_all = self.CC.get_stream_id(user_id, s)
                 stream_ids = []
 
+                # converts the dictionary to a list of stream ids
                 for stream_id in stream_id_all:
-                    stream_ids.append(stream_id['identifier'])  # converts the dictionar to a list of stream ids
+                    stream_ids.append(stream_id['identifier'])
 
-                for stream_id in stream_ids:  # for each stream id gets all the days
+                # for each stream id gets all the days
+                for stream_id in stream_ids:
                     stream_dicts = self.CC.get_stream_duration(stream_id)
                     stream_days = []
                     days = stream_dicts["end_time"] - stream_dicts["start_time"]
 
                     for day in range(days.days + 1):
-                        stream_days.append((stream_dicts["start_time"] + timedelta(days=day)).strftime('%Y%m%d'))
+                        stream_days.append((stream_dicts[
+                                                "start_time"] + timedelta(
+                            days=day)).strftime('%Y%m%d'))
 
-                    # creates a dictionary of stream ids for each stream where each stream id contains all the dates
-
+                    # creates a dictionary of stream ids for each stream,
+                    # where each stream id contains all the dates
                     if s == motionsense_hrv_accel_right:
-                        accel_right_stream_ids_with_date[stream_id] = stream_days
+                        accel_right_stream_ids_with_date[
+                            stream_id] = stream_days
                     elif s == motionsense_hrv_gyro_right:
                         gyro_right_stream_ids_with_date[stream_id] = stream_days
                     elif s == motionsense_hrv_accel_left:
@@ -104,19 +122,35 @@ class TypingMarker(ComputeFeatureBase):
                     elif s == motionsense_hrv_gyro_left:
                         gyro_left_stream_ids_with_date[stream_id] = stream_days
 
-        # creates unique days for the accelerometer left and accelerometer right
+        # creates unique days for the accl left and accl right data
+        accel_right_unique_days = unique_days_of_one_stream(
+            accel_right_stream_ids_with_date)
+        accel_left_unique_days = unique_days_of_one_stream(
+            accel_left_stream_ids_with_date)
 
-        accel_right_unique_days = unique_days_of_one_stream(accel_right_stream_ids_with_date)
-        accel_left_unique_days = unique_days_of_one_stream(accel_left_stream_ids_with_date)
-
-        # creates common days for the accelerometer left and accelerometer right
-
-        common_days = list(accel_right_unique_days.intersection(accel_left_unique_days))
+        # creates common days for the accl left and accl right data
+        common_days = list(
+            accel_right_unique_days.intersection(accel_left_unique_days))
         common_days.sort()
 
-        return common_days, accel_right_stream_ids_with_date, gyro_right_stream_ids_with_date, accel_left_stream_ids_with_date, gyro_left_stream_ids_with_date
+        return common_days, \
+               accel_right_stream_ids_with_date, \
+               gyro_right_stream_ids_with_date, \
+               accel_left_stream_ids_with_date, \
+               gyro_left_stream_ids_with_date
 
     def process(self, user, all_days):
+        """
+         This function processes both wrists' accl and gyro data for the
+         commons days to create data frames for left, right accl and gyro data.
+         Makes two dataframes for left(accl,gyro) and right(accl,gyro).
+         Then uses the sync funtion to sync left and right dataframes.
+         Finally uses the typing_episodes funtion to detect typing episodes.
+        :param user_id:
+        :param all_days:
+        :return: stores typing episodes data
+        """
+
         if self.CC is None:
             return
 
@@ -126,48 +160,84 @@ class TypingMarker(ComputeFeatureBase):
         streams = self.CC.get_user_streams(user)
 
         if not streams:
-            self.CC.logging.log("Typing Activity - no streams found for user: %s" %
-                                (user))
+            self.CC.logging.log(
+                "Typing Activity - no streams found for user: %s" %
+                (user))
             return
 
-        common_days, accel_right_stream_ids_with_date, gyro_right_stream_ids_with_date, accel_left_stream_ids_with_date, gyro_left_stream_ids_with_date = self.get_common_days(
-            user)
-        print(common_days)
+        common_days, accel_right_stream_ids_with_date, \
+        gyro_right_stream_ids_with_date, \
+        accel_left_stream_ids_with_date, gyro_left_stream_ids_with_date = \
+            self.get_common_days(user)
+
         for day in all_days:
             if day not in common_days:
                 continue
 
-            get_all_data = self.collect_data(accel_right_stream_ids_with_date, day, user)
+            # gets all the data for all stream ids of accl_right for a day
+            get_all_data = self.collect_data(accel_right_stream_ids_with_date,
+                                             day, user)
+
             if len(get_all_data) == 0:
                 continue
-            acc_dataR = get_dataframe(get_all_data, ['time', 'arx', 'ary', 'arz'])
 
-            get_all_data = self.collect_data(gyro_right_stream_ids_with_date, day, user)
+            # creats a dataframe with accl_right data of a day
+            acc_dataR = get_dataframe(get_all_data,
+                                      ['time', 'arx', 'ary', 'arz'])
+
+            # gets all the data for all stream ids of gyro_right for a day
+            get_all_data = self.collect_data(gyro_right_stream_ids_with_date,
+                                             day, user)
+
             if len(get_all_data) == 0:
                 continue
-            gyr_dataR = get_dataframe(get_all_data, ['time', 'grx', 'gry', 'grz'])
 
-            get_all_data = self.collect_data(accel_left_stream_ids_with_date, day, user)
+            # creats a dataframe with gyro_right data of a day
+            gyr_dataR = get_dataframe(get_all_data,
+                                      ['time', 'grx', 'gry', 'grz'])
+
+            # gets all the data for all stream ids of accl_left for a day
+            get_all_data = self.collect_data(accel_left_stream_ids_with_date,
+                                             day, user)
+
             if len(get_all_data) == 0:
                 continue
-            acc_dataL = get_dataframe(get_all_data, ['time', 'alx', 'aly', 'alz'])
 
-            get_all_data = self.collect_data(gyro_left_stream_ids_with_date, day, user)
+            # creats a dataframe with accl_left data of a day
+            acc_dataL = get_dataframe(get_all_data,
+                                      ['time', 'alx', 'aly', 'alz'])
+
+            # gets all the data for all stream ids of gyro_left for a day
+            get_all_data = self.collect_data(gyro_left_stream_ids_with_date,
+                                             day, user)
+
             if len(get_all_data) == 0:
                 continue
-            gyr_dataL = get_dataframe(get_all_data, ['time', 'glx', 'gly', 'glz'])
 
-            dr = pd.concat((acc_dataR[acc_dataR.columns[0:4]], gyr_dataR[gyr_dataR.columns[1:4]]), axis=1)
-            dl = pd.concat((acc_dataL[acc_dataL.columns[0:4]], gyr_dataL[gyr_dataL.columns[1:4]]), axis=1)
+            # creats a dataframe with gyro_left data of a day
+            gyr_dataL = get_dataframe(get_all_data,
+                                      ['time', 'glx', 'gly', 'glz'])
 
+            # merges left accl,gyro to make a single dataframe. Same thing done
+            # for right accl and gyro.
+            dr = pd.concat((acc_dataR[acc_dataR.columns[0:4]],
+                            gyr_dataR[gyr_dataR.columns[1:4]]), axis=1)
+            dl = pd.concat((acc_dataL[acc_dataL.columns[0:4]],
+                            gyr_dataL[gyr_dataL.columns[1:4]]), axis=1)
+
+            # syncs left and right dataframes
             dataset = sync_left_right_accel(dl, dr)
 
+            # gets the time offset data for the participant
             offset = 0
+
             if len(get_all_data) > 0:
                 offset = get_all_data[0].offset
+
+            # gets the typying episdoes information
             data = typing_episodes(dataset, offset)
 
-            self.store_stream(filepath='typing_episode_1_sec_window.json',
+            self.store_stream(filepath='typing_episode_800_milisec_window.json',
                               input_streams=[
                                   streams[motionsense_hrv_accel_right],
                                   streams[motionsense_hrv_gyro_right],
@@ -176,4 +246,5 @@ class TypingMarker(ComputeFeatureBase):
                               user_id=user,
                               data=data)
 
-        self.CC.logging.log("Finished processing Typing Activity for user: %s" % (user))
+        self.CC.logging.log(
+            "Finished processing Typing Activity for user: %s" % (user))
