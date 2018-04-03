9493d0c98556ed9d42b57657d41a0629aadaefdc
aungkonazim <aungkonazim@gmail.com>
2018-04-03 02:31:13 -0500

    added data yield

--- core/feature/data_yield/data_yield.py
+++ core/feature/data_yield/data_yield.py
@@ -22,4 +22,67 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from core.computefeature import ComputeFeatureBase
+from core.feature.data_yield.utils.util import *
+from core.signalprocessing.window import window_sliding
+
+feature_class_name = 'data_yield'
+
+class data_yield(ComputeFeatureBase):
+
+    def calculate_yield(self,user_id,stream_identifier,all_days,all_streams,json_path):
+        for day in all_days:
+            final_data = []
+            motionsense_raw = self.CC.get_stream(all_streams[stream_identifier]["identifier"],
+                                                 day=day,user_id=user_id,localtime=False)
+            if not list(motionsense_raw.data):
+                continue
+            data = admission_control(motionsense_raw.data)
+            if not list(data):
+                continue
+            decoded_data = decode_only(data)
+            offset = decoded_data[0].offset
+            windowed_decoded_data = window_sliding(decoded_data,window_size=window_size_60sec,
+                                                   window_offset=window_size_60sec)
+            final_yield = []
+            for key in windowed_decoded_data.keys():
+                window_data_60sec = windowed_decoded_data[key]
+                window_data_10sec = window_sliding(window_data_60sec,
+                                                   window_size=window_size_10sec,
+                                                   window_offset=window_size_10sec)
+                quality = get_quality(window_data_10sec,Fs)
+                final_yield.append(DataPoint.from_tuple(start_time=key[0],
+                                                        end_time=key[1],
+                                                        offset=offset,
+                                                        sample = quality))
+            if not final_yield:
+                continue
+            self.store_stream(json_path,
+                              [all_streams[stream_identifier]],
+                              user_id,
+                              final_yield,localtime=False)
+
+    def process(self, user, all_days):
+        """
+
+        :param user: user id string
+        :param all_days: list of days to compute
+
+        """
+        if not all_days:
+            return
+        if self.CC is None:
+            return
+        if not user:
+            return
+        all_streams = self.CC.get_user_streams(user_id=user)
+
+        if all_streams is None:
+            return
+        if motionsense_hrv_left not in all_streams and  motionsense_hrv_right not in all_streams:
+            return
+        user_id = user
+        json_path = 'data_yield.json'
+        self.calculate_yield(user_id,motionsense_hrv_left,all_days,all_streams,json_path)
+        self.calculate_yield(user_id,motionsense_hrv_right,all_days,all_streams,json_path)
 
