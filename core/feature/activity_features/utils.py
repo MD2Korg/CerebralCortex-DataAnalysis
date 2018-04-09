c8d6e19ee8379d2ef7fba9fd4d80a707691c39f4
sakther <sakther@memphis.edu>
2018-04-09 01:56:15 -0500

    Added activity type daily -Imputed with time of day -Imputed with time of day of week

--- core/feature/activity_features/utils.py
+++ core/feature/activity_features/utils.py
@@ -17,6 +17,26 @@ POSTURE_LABELS = [LYING, SITTING, STANDING]
 WALKING_HOURLY = 'walking_time_hourly.json'
 MODERATE_ACTIVITY_HOURLY = 'moderate_activity_time_hourly.json'
 HIGH_ACTIVITY_HOURLY = 'high_activity_time_hourly.json'
+
+WALKING_DAILY = 'walking_time_daily.json'
+MODERATE_ACTIVITY_DAILY = 'moderate_activity_time_daily.json'
+HIGH_ACTIVITY_DAILY = 'high_activity_time_daily.json'
+
+WALKING_IMPUTED_TIME_OF_DAY_DAILY = 'walking_time_imputed_time_of_day_daily.json'
+MODERATE_ACTIVITY_IMPUTED_TIME_OF_DAY_DAILY = 'moderate_activity_time_imputed_time_of_day_daily.json'
+HIGH_ACTIVITY_IMPUTED_TIME_OF_DAY_DAILY = 'high_activity_time_imputed_time_of_day_daily.json'
+
+WALKING_IMPUTED_DAY_OF_WEEK_DAILY = 'walking_time_imputed_day_of_week_daily.json'
+MODERATE_ACTIVITY_IMPUTED_DAY_OF_WEEK_DAILY = 'moderate_activity_time_imputed_day_of_week_daily.json'
+HIGH_ACTIVITY_IMPUTED_DAY_OF_WEEK_DAILY = 'high_activity_time_imputed_day_of_week_daily.json'
+
+
 LYING_HOURLY = 'lying_time_hourly.json'
 SITTING_HOURLY = 'sitting_time_hourly.json'
 STANDING_HOURLY = 'standing_time_hourly.json'
+
+from datetime import timedelta, timezone, datetime
+
+def get_datetime_with_tz(year, month, day, hour=0, min=0, second=0, offset=0):
+    tz = timezone(timedelta(milliseconds=offset))
+    return datetime(year, month, day, hour, min, second, tzinfo=tz)
