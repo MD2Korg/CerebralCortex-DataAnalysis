35002f840caa370eb544d2f2e79381d7f9907d13
aungkonazim <aungkonazim@gmail.com>
2018-04-13 00:37:01 -0500

    added stress from wrist comments

--- core/feature/stress_from_wrist/stress_from_wrist.py
+++ core/feature/stress_from_wrist/stress_from_wrist.py
@@ -31,14 +31,83 @@ from scipy.stats import iqr
 from scipy.stats import skew
 from scipy.stats import kurtosis
 from copy import deepcopy
-import sklearn
+
 feature_class_name = 'stress_from_wrist'
 
 class stress_from_wrist(ComputeFeatureBase):
-
+    """
+    This class extracts all the RR interval data that is present for a person on a specific day.
+    Calculates the necessary features from the rr interval data and assigns each minute of the day as 
+    Stress/Not stress state.
+    
+    ..algorithm:
+        Input:
+            RR interval datastream. 
+            Each datapoint contains the following three things
+                1. A list of RR-interval array. Each entry in the list corresponds to a realization 
+                of the position of R peaks in that minute
+                2. Standard Deviation of Heart Rate within the minute
+                3. A list corresponding to the heart rate values calculated from variable realizations 
+                of the RR interval on a sliding window of window size = 8 second and window offset = 2 second.
+        Steps:
+            1. Extract all the RR interval data for a user on a specific day
+            2. Extract all the 16 features per minute of the RR interval data
+            3. Standardize each feature row and output stress/not stress state
+        
+        Output:
+            A datastream containing a list of datapoints, each datapoint represents one minute where sample=0 
+            means the user was not stressed and sample=1 means the person was stressed
+        List of Features:
+            1. 82nd perentile
+            2. 18th perentile
+            3. mean
+            4. median
+            5. standard deviation
+            6. inter quartile deviation
+            7. skewness
+            8. kurtosis
+            9. Energy in very low frequency range
+            10. Energy in very high frequency range
+            11. Energy in low frequency range
+            12. Ration of low to high frequency energy 
+            13. quartile deviation
+            14. heart rate
+            15. median of inter percentile difference
+            16. standard deviation of inter percentile difference 
+        References:
+            K. Hovsepian, M. al’Absi, E. Ertin, T. Kamarck, M. Nakajima, and S. Kumar, 
+            "cStress: Towards a Gold Standard for Continuous Stress Assessment in the Mobile Environment," 
+            ACM UbiComp, pp. 493-504, 2015.
+    
+    """
 
     def get_feature_for_one_window(self,
                                    rr:list)->np.ndarray:
+        """
+        This function takes as input a list of many realizations of where the R peaks are present in one minute of 
+        PPG data and calculates 16 features from it.
+        The features are:
+            1. 82nd perentile
+            2. 18th perentile
+            3. mean
+            4. median
+            5. standard deviation
+            6. inter quartile deviation
+            7. skewness
+            8. kurtosis
+            9. Energy in very low frequency range
+            10. Energy in very high frequency range
+            11. Energy in low frequency range
+            12. Ration of low to high frequency energy 
+            13. quartile deviation
+            14. heart rate
+            15. median of inter percentile difference
+            16. standard deviation of inter percentile difference 
+        
+        :param rr: A list of numpy array. Each array contains a realization of RR interval timeseries for the same one 
+        minute of data 
+        :return: A numpy array of shape(1,16) representing the 16 features calculated from one minute of data
+        """
 
         temp = np.zeros((len(rr),no_of_feature))
         for k,rr_list in enumerate(rr):
@@ -73,9 +142,28 @@ class stress_from_wrist(ComputeFeatureBase):
                           day:str,
                           stream_identifier:str,
                           user_id:str,
+                          json_path:str,
                           model,
-                          scaler,
-                          json_path:str):
+                          scaler):
+        """
+        This  function takes all the streams of one user and extracts all the RR interval data the person has for a 
+        specific day and calculates a feature matrix of shape (m,16) where m is the number of minutes of rr interval data
+        present for the day.
+        
+        It then transforms the feature matrix on a row by row basis with a pre trained standard transformation and 
+        applies the stess model to get a binary output of 0/1 meaning stress/not stress.
+        
+        It saves the stress datastream
+        
+        :param streams: All the streams of a user 
+        :param day: day in string format
+        :param stream_identifier: stream name of rr interval
+        :param user_id: uuid of user
+        :param json_path: the name of the json file where the metadata of stress from wrist is written
+        :param model: a sklearn logistic regression model trained  
+        :param scaler: a sklearn standard transformation trained to standardize the feature the feature values
+        
+        """
 
         rr_interval_data = self.CC.get_stream(streams[stream_identifier]["identifier"],
                                           day=day,user_id=user_id,localtime=False)
@@ -128,7 +216,7 @@ class stress_from_wrist(ComputeFeatureBase):
         json_path = 'stress_wrist.json'
         model,scaler = get_model()
         for day in all_days:
-            self.get_and_save_data(streams,day,rr_interval_identifier,user_id,model,scaler,json_path)
+            self.get_and_save_data(streams,day,rr_interval_identifier,user_id,json_path,model,scaler)
 
 
 
