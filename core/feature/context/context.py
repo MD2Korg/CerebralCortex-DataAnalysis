d8f2365492afe63d8e02a099f45216fc33c9d13e
nasirali1 <nasir.ali08@gmail.com>
2018-05-08 15:32:52 -0500

    test code

--- core/feature/context/context.py
+++ core/feature/context/context.py
@@ -39,9 +39,6 @@ class Context(ComputeFeatureBase, ContextInteraction, ContextWhere, ContextActiv
     """
     Detect whether a person was interacting with other people immediately before filling qualtrics survey.
     """
-    def __init__(self):
-        pass
-
 
     def get_day_data(self, user_id, stream_name, day, start_time, end_time):
         """
