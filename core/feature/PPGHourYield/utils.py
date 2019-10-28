82c46ac833da6907759959313b2166c29cc32cad
aungkonazim <aungkonazim@gmail.com>
2019-10-28 09:24:27 -0500

    PPG Yield  (#127)
    * added rr_interval changes
    
    * some change
    
    * added rr interval computation ability for sobclab
    
    * changed rr interval stream name
    
    * changed rr_interval_code to get the time information also
    
    * added version 2
    
    * uncommented the daily presence in rr interval
    
    * completed the feature
    
    * some necessary change
    
    * added a change to json path var
    
    * finalized saving
    
    * added print statements
    
    * made some changes to code
    
    * made changes to one parameter and changed the name of datastream to be computed
    
    * added dataquality ppg
    
    * added metadata for ppg data quality
    
    * added PPG data yield hourly
    
    * changed the motion classifier
    
    * added a fix

---
+++ core/feature/PPGHourYield/utils.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2019, MD2K Center of Excellence
+# All rights reserved.
+# author: Md Azim Ullah
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from sklearn.ensemble import RandomForestClassifier
+from core.computefeature import get_resource_contents
+import pickle
+MODEL_FILENAME = 'core/resources/models/motion/clf_motion.p'
+
+def get_model() -> RandomForestClassifier:
+    """
+
+    :rtype: object
+    :param bool is_gravity:
+    :return:
+    """
+    model_file_contents = get_resource_contents(MODEL_FILENAME)
+    clf = pickle.loads(model_file_contents)
+    return clf
